40f5a4ee5dd35093f222923e3cb71d4632a90d12
Abdallah_RT <113673907+AbdallahRT@users.noreply.github.com>
2026-02-05 10:38:14 +0400

    Time: 0 ms (100%), Space: 25.7 MB (70.75%) - LeetHub

---
+++ 3651-transformed-array/3651-transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int >ans(n);
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)ans[i]=0;
+            else{
+                int pos= ((i+nums[i])%n+n) %n;
+                ans[i]=nums[pos];
+            }
+        }
+
+        return ans;
+    }
+};
