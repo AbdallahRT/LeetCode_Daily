aaa0a5fc7910e5ea2c2c0c9b018da84a33656f1c
Abdallah_RT <113673907+AbdallahRT@users.noreply.github.com>
2026-05-07 12:34:46 +0300

    Time: 9 ms (72.71%), Space: 56.7 MB (90.1%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int n = boxGrid.size();
+        int m = boxGrid[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            int tmp=m-1;
+            for(int j=m-1;j>=0;j--)
+            {
+                if(boxGrid[i][j]=='*') tmp=j-1;
+                else if(boxGrid[i][j]=='#'){
+                    swap(boxGrid[i][j],boxGrid[i][tmp--]);
+                }
+            }
+        }
+
+        vector<vector<char>> ans(m,vector<char>(n));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans[j][n-i-1]=boxGrid[i][j];
+            }
+        }
+
+        return ans;
+       
+    }
+};
